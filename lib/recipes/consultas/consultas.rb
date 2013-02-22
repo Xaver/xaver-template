a8921d05b75e784fa0a7eda265c1c150749ef591
Gustavo Molinari <gustavo.molinari@xaver.com.ar>
2013-02-22 16:09:26 -0300

    Cambios

--- lib/recipes/consultas/consultas.rb
+++ lib/recipes/consultas/consultas.rb
@@ -3,7 +3,7 @@
 puts "\nConfigurando consultas..."
 directory @directorio, File.join("..", @app_name)
 
-insert_into_file "app/views/admin/admin/_nav_lateral.html.erb", "\n<li><%= link_to 'Consultas', admin_consultas_path %></li>", :after => '<li class="nav-header">General</li>'
+insert_into_file "app/views/admin/admin/_nav_lateral.html.erb", "\n<li><%= link_to 'Consultas', admin_consultas_path if can? :index, Consulta %></li>", :after => '<li class="nav-header">General</li>'
 
 insert_into_file "config/routes.rb", :after => "namespace :admin do" do
   "\nresources :consultas, :only => [:index, :show, :destroy]\n"
